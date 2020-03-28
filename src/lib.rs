91341ddfb175382b4d1a8bd51bbb7a339acba7e4
Doga Yuksel <dogayuksel@pm.me>
2020-03-28 18:46:33 +0100

    better scoring for matches

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,11 @@
-use std::collections::{ BTreeMap, HashSet, HashMap };
+use std::cmp::Ordering::Equal;
+use std::collections::{BTreeMap, HashMap, HashSet};
 
-use wasm_bindgen::prelude::*;
 use js_sys::try_iter;
+use wasm_bindgen::prelude::*;
 
-use fst::{IntoStreamer, Streamer, Set, SetBuilder};
 use fst::automaton::Levenshtein;
+use fst::{IntoStreamer, Set, SetBuilder, Streamer};
 
 mod utils;
 
@@ -15,13 +16,13 @@ mod utils;
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
 #[wasm_bindgen]
-extern {
+extern "C" {
     fn alert(s: &str);
 }
 
 #[wasm_bindgen]
 pub struct FuzzyMatcherBuilder {
-    set_builder: SetBuilder<Vec<u8>>, 
+    set_builder: SetBuilder<Vec<u8>>,
 }
 
 impl FuzzyMatcherBuilder {
@@ -41,7 +42,7 @@ impl FuzzyMatcherBuilder {
 }
 
 pub struct WordIndex {
-    collection: BTreeMap::<String, HashSet<usize>>,
+    collection: BTreeMap<String, HashSet<usize>>,
 }
 
 impl WordIndex {
@@ -56,7 +57,7 @@ impl WordIndex {
             Some(value) => {
                 value.insert(index);
                 ();
-            },
+            }
             None => {
                 let mut new_set = HashSet::new();
                 new_set.insert(index);
@@ -73,7 +74,7 @@ impl WordIndex {
 
 #[wasm_bindgen]
 pub struct FuzzyMatcher {
-    set: Set<Vec<u8>>,    
+    set: Set<Vec<u8>>,
     word_index: WordIndex,
 }
 
@@ -86,7 +87,7 @@ impl FuzzyMatcher {
         let iterator = try_iter(js_phrases).unwrap().unwrap();
         for (ind, x) in iterator.enumerate() {
             let phrase = x.unwrap().as_string().unwrap();
-            for word in phrase.split_whitespace() {
+            for word in phrase.split_whitespace().map(|w| w.to_lowercase()) {
                 word_index.add_key(word.to_string(), ind);
             }
         }
@@ -101,33 +102,42 @@ impl FuzzyMatcher {
     }
 
     pub fn query(&self, keywords: String) -> String {
-        let mut count_matches: HashMap<usize, u32> = HashMap::new();
-        for keyword in keywords.split_whitespace() {
-            for distance in 0..=2 {
+        let mut count_matches: HashMap<usize, f32> = HashMap::new();
+        for keyword in keywords.split_whitespace().map(|k| k.to_lowercase()) {
+            for distance in 0..=4 {
                 match Levenshtein::new(&keyword, distance) {
                     Ok(lev) => {
                         let mut stream = self.set.search(&lev).into_stream();
                         while let Some(k) = stream.next() {
                             let match_as_string = std::str::from_utf8(k).unwrap();
                             let indexes = self.word_index.get(&match_as_string);
-                            let increment = match distance {
-                                0 => 7,
-                                1 => 3,
-                                2 => 1,
-                                _ => 0,
+                            let increment = match (
+                                distance,
+                                (keyword.len() as f32)
+                            ) {
+                                (0, x) => 11.0 * x.sqrt(),
+                                (1, x) if x >= 3.0 => 7.0 * x.sqrt(),
+                                (2, x) if x >= 5.0 => 5.0 * x.sqrt(),
+                                (3, x) if x >= 7.0 => 2.0 * x.sqrt(),
+                                (4, x) if x >= 9.0 => x.sqrt(),
+                                _ => 0.0,
                             };
-                            for index in indexes {
-                                *count_matches.entry(*index).or_insert(0) += increment;
+                            if increment != 0.0 {
+                                for index in indexes {
+                                    *count_matches.entry(*index).or_insert(0.0) += increment;
+                                }
                             }
                         }
                     }
-                    Err(_) => { break; }
+                    Err(_) => {
+                        break;
+                    }
                 };
             }
         }
-        let mut count_vec: Vec<(&usize, &u32)> = count_matches.iter().collect();
-        count_vec.sort_by(|a, b| b.1.cmp(a.1));
-        let mut response = vec!();
+        let mut count_vec: Vec<(&usize, &f32)> = count_matches.iter().collect();
+        count_vec.sort_by(|a, b| b.1.partial_cmp(a.1).unwrap_or(Equal));
+        let mut response = vec![];
         for (index, _) in &count_vec {
             response.push(index.to_string());
         }
